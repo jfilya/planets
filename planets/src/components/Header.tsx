7bcf26c97be752b447b193df147a34ea33886ac8
Julia Filippova <julia.94@bk.ru>
2023-03-09 16:20:49 +0300

    feat: add highlight links

--- planets/src/components/Header.tsx
+++ planets/src/components/Header.tsx
@@ -1,40 +1,53 @@
 import React from "react";
 import { Link } from "react-router-dom";
-class Header extends React.Component {
-  render() {
-    return (
-      <header className="header">
-        <ul>
+const Header = () => {
+  const [items, setItems] = React.useState(0);
+  React.useEffect(() => {
+  const links = document.querySelectorAll('li');
+  links[items].classList.add('active-planets')
+  links.forEach((link, index, array) => {
+    link.onclick = () => {
+      setItems(index);
+      array.forEach(el => {
+        el.classList.remove('active-planets')
+      })
+    }
+  })
+  
+  
+});
+  return (
+    <header className="header">
+      <ul>
         <li>
-            <Link to="/">Solar</Link>
-          </li>
-          <li>
-            <Link to="/Mercury-palent">Mercury</Link>
-          </li>
-          <li>
-            <Link to="/Venus-palent">Venus</Link>
-          </li>
-          <li>
-            <Link to="/Earth-palent">Earth</Link>
-          </li>
-          <li>
-            <Link to="/Mars-palent">Mars</Link>
-          </li>
-          <li>
-            <Link to="/Jupiter-palent">Jupiter</Link>
-          </li>
-          <li>
-            <Link to="/Saturn-palent">Saturn</Link>
-          </li>
-          <li>
-            <Link to="/Uranus-palent">Uranus</Link>
-          </li>
-          <li>
-            <Link to="/Neptune-palent">Neptune</Link>
-          </li>
-        </ul>
-      </header>
-    );
-  }
-}
+          <Link to="/">Solar</Link>
+        </li>
+        <li>
+          <Link to="/Mercury-palent">Mercury</Link>
+        </li>
+        <li>
+          <Link to="/Venus-palent">Venus</Link>
+        </li>
+        <li>
+          <Link to="/Earth-palent">Earth</Link>
+        </li>
+        <li>
+          <Link to="/Mars-palent">Mars</Link>
+        </li>
+        <li>
+          <Link to="/Jupiter-palent">Jupiter</Link>
+        </li>
+        <li>
+          <Link to="/Saturn-palent">Saturn</Link>
+        </li>
+        <li>
+          <Link to="/Uranus-palent">Uranus</Link>
+        </li>
+        <li>
+          <Link to="/Neptune-palent">Neptune</Link>
+        </li>
+      </ul>
+    </header>
+  );
+};
 export default Header;
